df2e8da7cbfb568b9fa7dbd87a34c63efd209a4d
Cyverufo44 <obelejunior10@gmail.com>
2023-11-21 13:02:23 -0800

    lets go there

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-square.py defines a sqaure
+"""a class for a square"""
+
+
+class Square:
+        """Does nothing"""
+            pass
+
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+re class that creates a private attribute"""
+
+
+class Square:
+        """creates a private attributes"""
+            def __init__(self, size):
+                    self.__size = size
