9ed9fec8a8967bcbf97981e937824bec59ec1aed
Cyverufo44 <obelejunior10@gmail.com>
2023-11-21 14:57:05 -0800

    Even more bugs

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,8 @@
-ss that creates a private attribute"""
-
-
+#!/usr/bin/python3
 class Square:
-        """private attribute"""
             def __init__(self, size=0):
-                    """Initializing this square class
-                            Args:
-                                            size: represnets the size of the square defined
-                                                    Raises:
-                                                                    TypeError: if size is not integer
-                                                                                ValueError: if size is less than zero
-                                                                                        """
-                                                                                                if not isinstance(size, int):
-                                                                                                            raise TypeError("size must be an integer")
-                                                                                                                    if size < 0:
-                                                                                                                                raise ValueError("size must be >= 0")
-                                                                                                                                        self.__size = size
+                        if not isinstance(size, int):
+                                        raise TypeError("size must be an integer")
+                        if size < 0:
+                                        raise ValueError("size must be >= 0")
+                        self.__size = size
