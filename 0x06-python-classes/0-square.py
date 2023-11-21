98110ebeda9bc815648abf3996e8ecd8839aca1d
Cyverufo44 <obelejunior10@gmail.com>
2023-11-21 13:36:45 -0800

    fix the bugs

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-# 0-square.py defines a sqaure
-"""a class for a square"""
-
-
+# 0-square.py defines a square
+"""A class for a square"""
 class Square:
         """Does nothing"""
-            pass
-
+        pass
